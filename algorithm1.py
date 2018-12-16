c4e2273135ce1be5dcebadcc50ca97c2c039b59a
psacci <pedro_sacci@hotmail.com>
2018-12-16 01:24:40 -0200

    prim done

--- algorithm1.py
+++ algorithm1.py
@@ -30,7 +30,10 @@ class Graph:
                     self.data.at[cont-1,'ORIGEM'] = nodos[0]
                     self.data.at[cont-1,'DESTINO'] = nodos[1]
                     self.data.at[cont-1,'DISTANCIA'] = nodos[2]
-                    cont +=1
+                    self.data.at[cont,'ORIGEM'] = nodos[1]
+                    self.data.at[cont,'DESTINO'] = nodos[0]
+                    self.data.at[cont,'DISTANCIA'] = nodos[2]
+                    cont +=2
                     line = fp.readline()
                     
         #monta lista de nodos do grafo, para facilitar algoritmo   
@@ -61,33 +64,35 @@ class Graph:
         for i in range(len(self.nodos)):
             if int(self.nodos[i]) == int(self.nodos[0]):
                 distancias[i] = 0
+                foi_escolhido[i] = True
         #nodo_atual sera um dos nodos da lista de nodos origem
         nodos_origem = [self.nodos[0]]
         nodo_atual = nodos_origem[0]
-        all_nodes_visited = False
-        print ("Caminho percorrido")  
+        print("Nodo inicial: vertice "+str(nodo_atual))
+        all_nodes_visited = False 
         
         while all_nodes_visited == False:
         
         #funcionamento parecido com dijkstra, a diferença é que haverá mais nodos origens ao longo da iteração... (lista de nodos visitados aumenta)
-            print (nodo_atual)
+
+
             for i in range(len(distancias)):
                 destino_iterativo = self.nodos[i]
-                
+                    
                 distancia_da_aresta = 0
                 for j in range (len(self.data["ORIGEM"])):
                     if self.data.at[j,"ORIGEM"] == nodo_atual and self.data.at[j,"DESTINO"] == destino_iterativo:
                         distancia_da_aresta = self.data.at[j,"DISTANCIA"]
-                    
-            
+                        
+                
                 #se tiver origem->destino no dataframe, portanto se tiver aresta
                 if distancia_da_aresta != 0:
                     #tem um numero já, checar se deve atualizar (checar se distancias[i] > distancia no dataframe)                  
                     if distancias[i] != 0 and distancias[i] != inf:
-                        if int(distancias[i]) > int(distancia_da_aresta):
+                        if int(distancias[i]) > int(distancia_da_aresta) and foi_escolhido[i] == False:
                             #atualiza
                             distancias[i] = int(distancia_da_aresta)
-                            
+                                
                     #valor atual eh infinito, entao qualquer valor eh menor e pode atualizar
                     if distancias[i] == inf:
                         distancias[i] = int(distancia_da_aresta)
@@ -102,20 +107,23 @@ class Graph:
                 elif distancias[i] != 0 and float(menor_numero) > float(distancias[i]) and foi_escolhido[i] == False:
                     menor_numero = distancias[i]
                     index = i
-            
+
+            #print("Aresta do vertice "+str(nodo_atual)+ " ate vertice "+str(self.nodos[index]))
+            #print(distancias)
             foi_escolhido[index] = True
+            #print (foi_escolhido)
 
             #atualizar o nodos_origem para o nodo com menor distancia na lista
             nodos_origem.append(self.nodos[index])
+            nodo_atual = self.nodos[index]
             #checar se deve acabar o algoritmo, vendo se todos nodos foram visitados
             if len(nodos_origem) == len(self.nodos):
                 all_nodes_visited = True
-                print (nodo_atual)
 
         menor_comprimento = 0
-        for i in range(distancias):
+        for i in range(len(distancias)):
             menor_comprimento = distancias[i] + menor_comprimento
-        #retornar shortest_path
+        #retornar comprimento
         return menor_comprimento                  
         
         
